5e9c97ffc0da01cd5150a1f2441243c0882840d1
Venkatesh89-Github <56060159+Venkatesh89-Github@users.noreply.github.com>
2022-08-04 14:31:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 module "resourcegroup" {
   source  = "app.terraform.io/example-org-2888e9/resourcegroup/azurerm"
   version = "1.0.0"
+  name    = var.name
+  location = var.location
   # insert required variables here
 }
