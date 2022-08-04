37fd2aa98aabe0e498de8514d742f78800f8d6ed
Venkatesh89-Github <56060159+Venkatesh89-Github@users.noreply.github.com>
2022-08-04 14:24:35 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,4 +1,16 @@
-  name     = var.name
-  location = var.location
 
-  tags = var.tags
+variable "location" {
+  description = <<EOT
+The Azure Region where the Resource Group should exist.
+Changing this forces a new Resource Group to be created.
+EOT
+  type        = string
+}
+
+variable "name" {
+  description = <<EOT
+The Name which should be used for this Resource Group.
+Changing this forces a new Resource Group to be created.
+EOT
+  type        = string
+}
