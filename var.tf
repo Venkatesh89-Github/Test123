3dcf99ff4aa2359c12ea0caaedd7520d3ac20ea4
Venkatesh89-Github <56060159+Venkatesh89-Github@users.noreply.github.com>
2022-08-04 14:35:59 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,5 +1,5 @@
 
-variable "location" {
+/* variable "location" {
   description = <<EOT
 The Azure Region where the Resource Group should exist.
 Changing this forces a new Resource Group to be created.
@@ -13,4 +13,4 @@ The Name which should be used for this Resource Group.
 Changing this forces a new Resource Group to be created.
 EOT
   default     = "East US"
-}
+}*/
