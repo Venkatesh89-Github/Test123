c9232d50103a4a9f561068c2ce205d2eeaaea12c
Venkatesh89-Github <56060159+Venkatesh89-Github@users.noreply.github.com>
2022-08-04 18:33:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ provider "azurerm" {
 module "resourcegroup" {
   source  = "app.terraform.io/example-org-2888e9/resourcegroup/azurerm"
   version = "1.0.0"
-  name    = "venky-tf"
+  name    = "venky-tf2"
   location = "East US"
   # insert required variables here
 }
